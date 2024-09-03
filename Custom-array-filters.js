2df8c0c655221c44a3ce3202ca3515267fc0e42a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-02 19:41:34 -0700

    Update Custom-array-filters.js

--- Custom-array-filters.js
+++ Custom-array-filters.js
@@ -20,3 +20,26 @@ Note: List with non-numbers will be tested as well.
 
 
 // Solution
+
+Array.prototype.int = function (){
+  return this.filter(function (x) { return typeof x == 'number' && x == ~~x });
+}
+Array.prototype.even = function(){
+  return this.int().filter(function (x) { return ~x & 1 });
+}
+
+Array.prototype.odd = function(){
+  return this.int().filter(function (x) { return x & 1 });
+}
+
+Array.prototype.under = function(x){
+  return this.int().filter(function (y) { return y < x });
+}
+
+Array.prototype.over = function(x){
+  return this.int().filter(function (y) { return y > x });
+}
+
+Array.prototype.inRange = function(min,max){
+  return this.int().filter(function (x) { return x >= min && x <= max });
+}
